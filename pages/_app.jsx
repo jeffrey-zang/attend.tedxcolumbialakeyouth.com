41ae8e987c6b5ff0ac7820d0d0564c118f546090
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-07-18 20:49:27 -0400

    add metadata

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,15 @@
 import '../styles/styles.scss'
 import Layout from './layout'
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Attend | TEDxColumbia Lake Youth</title>
+        <meta name="description" content="Application portal for TEDxColumbia Lake Youth"></meta>
+        <link rel="icon" href="/tedx.png" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
