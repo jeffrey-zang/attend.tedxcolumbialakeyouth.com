723236ecb0591e3748156d46cec44833e25456c6
Jeffrey Zang <jeffrey.zang.07@gmail.com>
2023-10-01 13:50:42 -0400

    chore: close forms

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,47 +7,47 @@ export default function Home() {
 
   return (
     <main>
-      <div onClick={() => router.push('/register/attendee')} className='p-10 container cursor-pointer opacity-100 hover:opacity-75 transition-opacity'>
+      <div className='p-10 container cursor-not-allowed opacity-75 transition-opacity'>
         <div className='flex justify-between items-center md:flex-row flex-col'>
           <div>
-            <p className='text-dimmed'>DEADLINE SEP 30</p>
+            <p className='text-dimmed'>EXPIRED</p>
             <h1 className='text-2xl mt-2'>Attendee Form</h1>
             <p className='mt-6'>Register to be an attendee and contribute to our mission of reinventing education.</p>
           </div>
-          <button onClick={() => router.push('/register/attendee')} className='flex items-center gap-1 text-lg hover:gap-3 mt-4 md:mt-0 w-full md:w-auto'>
+          <button className='flex items-center gap-1 text-lg cursor-not-allowed mt-4 md:mt-0 w-full md:w-auto'>
             I want to attend
             <BiRightArrowAlt />
           </button>
         </div>
       </div>
 
-      {/* <div onClick={() => router.push('/register/speaker')} className='p-10 container cursor-pointer opacity-100 hover:opacity-75 transition-opacity mt-8'>
+      <div className='p-10 container cursor-not-allowed opacity-75 transition-opacity mt-8'>
         <div className='flex justify-between items-center md:flex-row flex-col'>
           <div>
-            <p className='text-dimmed'>DEADLINE SEP 14</p>
+            <p className='text-dimmed'>EXPIRED</p>
             <h1 className='text-2xl mt-2'>Speaker Application</h1>
             <p className='mt-6'>Make a direct impact on the youth thinkers and innovators of the future.</p>
           </div>
-          <button onClick={() => router.push('/register/speaker')} className='flex items-center gap-1 text-lg hover:gap-3 mt-4 md:mt-0 w-full md:w-auto'>
+          <button className='flex items-center gap-1 text-lg cursor-not-allowed mt-4 md:mt-0 w-full md:w-auto'>
             I want to speak
             <BiRightArrowAlt />
           </button>
         </div>
-      </div> */}
+      </div>
 
-      {/* <div onClick={() => router.push('/register/performance')} className='p-10 container cursor-pointer opacity-100 hover:opacity-75 transition-opacity mt-8'>
+      <div className='p-10 container cursor-not-allowed opacity-75 transition-opacity mt-8'>
         <div className='flex justify-between items-center md:flex-row flex-col'>
           <div>
-            <p className='text-dimmed'>DEADLINE SEP 14</p>
+            <p className='text-dimmed'>EXPIRED</p>
             <h1 className='text-2xl mt-2'>Performance Application</h1>
             <p className='mt-6'>Want to express your art through a performance at our event? This form is for you!</p>
           </div>
-          <button onClick={() => router.push('/register/performance')} className='flex items-center gap-1 text-lg hover:gap-3 mt-4 md:mt-0 w-full md:w-auto'>
+          <button className='flex items-center gap-1 text-lg cursor-not-allowed mt-4 md:mt-0 w-full md:w-auto'>
             I want to perform
             <BiRightArrowAlt />
           </button>
         </div>
-      </div> */}
+      </div>
     </main>
   )
 }
